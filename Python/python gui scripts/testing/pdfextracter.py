40a3b1aebe491a3eef566476381f5881b138c25d
WorldTeacher <kirchneralexander020@gmail.com>
2022-04-08 21:30:04 +0200

    start following tutorial

--- Python/python gui scripts/testing/pdfextracter.py
+++ Python/python gui scripts/testing/pdfextracter.py
@@ -1,5 +1,29 @@
 import tkinter as tk
-import pyPDF2 as pdf
-from PIL import  Image, ImageTk
+from tkinter import *
+import PyPDF2
+import glob
+from PIL import Image, ImageTk
+#setting variables
+imagefile=r"J:\GitHub\learning\Python\python gui scripts\testing\img.png"
+root = tk.Tk()
+canvas= tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+#load logo
+logo=Image.open(imagefile)
+logo=ImageTk.PhotoImage(logo)
+logo_label=tk.Label(image=logo)
+logo_label.image=logo
+logo_label.grid(column=1, row=0)
 
-print("Hello World")
+#instruction 
+instruction=tk.Label(root, text="select the pdf file you want to extract the content from")
+instruction.grid(columnspan=3, row=1)
+
+#browse button
+browse_text=tk.StringVar()
+browse_button=tk.Button(root, textvariable="browse text", bg="#20bebe", fg="white", height=2, width=15)
+browse_text.set("Browse Text")
+browse_button.grid(column=1, row=2)
+#gui
+root.mainloop()
+
